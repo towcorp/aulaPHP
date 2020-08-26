63a784c1ec77748d8308978db175a5382f29f99c
towcorp <towcorp@hotmail.com>
2020-08-26 16:18:48 -0300

    adicionando validacaode campos do formulario

--- script.php
+++ script.php
@@ -10,7 +10,28 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+if(empty($nome) || empty($idade))
+{
+    echo "O campo não pode ser vazio";
+    return;
+}
+if(strlen($nome) < 3 )
+{
+    echo 'O nome deve conter mais de 3 caracteres';
+    return;
+}
 
+if(strlen($nome) > 40)
+{
+    echo "O nome é muito extenso";
+    return;
+}
+
+if(!is_numeric($idade))
+{
+    echo "Informe um numero para a idade";
+    return;
+}
 
 if($idade >= 6 && $idade <= 12)
 {
